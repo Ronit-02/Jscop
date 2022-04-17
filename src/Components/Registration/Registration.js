be4f5402008e2059798c834288de82eff9da683f
Naman Gupta <75092271+namangupta06@users.noreply.github.com>
2022-04-17 23:02:39 +0530

    registration page edited

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -3,9 +3,22 @@ import './Registration.css'
 function Registration() {
   return (
     <div className='register'>
-        <div className="container">
-  <h1 class="neon">Registrations Starting Soon❗</h1>
-</div>
+      <img src="events-pass-1.png" className='pass-photo' alt="" />
+      <div className="benefit">
+      <h2 className='benifit-text'>Benefits</h2>
+      <ul>
+        <li>Lunch +Tea for 2 days</li>
+        <li>Exclusive access to all events and lectures</li>
+        <li>Best selling stock market course worth INR 999</li>
+        <li>Goodies and swags for every participant</li>
+      </ul>
+
+      </div>
+      <div className="blink">
+       <div className="first"><h3>Get your Exclusive pass in only <strike>199</strike> 149 only</h3></div>
+      <div className="second"><h3>*Early bird discount for first 75 Registrations</h3></div> 
+      <button className='btn-primary'>AVAIL YOUR PASS</button>
+      </div>
     </div>
   )
 }
