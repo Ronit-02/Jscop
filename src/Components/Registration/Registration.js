014c18777931b00d28c29ffa59d8b485a41eee7b
Naman Gupta <75092271+namangupta06@users.noreply.github.com>
2022-05-05 00:57:43 +0530

    Update Registration.js

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -18,13 +18,6 @@ function Registration() {
       </div>
       <div className="blink">
 <div class="razorpay-embed-btn" data-url="https://pages.razorpay.com/pl_JMNycTXgOsWZEW/view" data-text="Avail your pass now" data-color="#528FF0" data-size="large">
-  <script>
-    (function(){
-      var d=document; var x=!d.getElementById('razorpay-embed-btn-js')
-      if(x){ var s=d.createElement('script'); s.defer=!0;s.id='razorpay-embed-btn-js';
-      s.src='https://cdn.razorpay.com/static/embed_btn/bundle.js';d.body.appendChild(s);} else{var rzp=window['_rzp_'];
-      rzp && rzp.init && rzp.init()}})();
-  </script>
 </div>
       </div>
     </div>
